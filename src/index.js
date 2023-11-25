ee6e95f10f10d4e0f8c957bf62ccaf6d89efb051
VitorSantosH <wytorh@gmail.com>
2023-11-25 19:05:00 -0300

    initial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const http = require('http');
+const app = express();
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('common'));
+app.use('/', express.static('dist'))
+
+const portHttp = 3000;
+
+const httpServer = http.createServer(app);
+
+
+httpServer.listen(portHttp, function () {
+    console.log("JSON Server is running on " + portHttp);
+});

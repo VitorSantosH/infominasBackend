ab4c4b743cf1281b7325fb845741a93a7db2e9f9
VitorSantosH <wytorh@gmail.com>
2023-11-25 22:48:15 -0300

    correcao frescura http - https

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const http = require('http');
 const app = express();
-
+const axios = require('axios')
 
 
 app.use(cors());
@@ -11,6 +11,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('common'));
 app.use('/', express.static('dist'))
+app.get('/heros', async (req, res) => {
+
+    await axios.get('http://homologacao3.azapfy.com.br/api/ps/metahumans')
+        .then(responseAzapfy => {
+            res.send(responseAzapfy.data)
+        })
+        .catch(err => {
+            return res.send(err)
+        })
+
+})
+
 
 const portHttp = 8050;
 
